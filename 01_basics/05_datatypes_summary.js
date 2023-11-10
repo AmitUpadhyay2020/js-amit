90f98ede7bfe3ab72fabed288ab0cce90d65cfd6
AmitUpadhyay2020 <149964489+AmitUpadhyay2020@users.noreply.github.com>
2023-11-10 15:59:45 +0000

    mychange

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -1,6 +1,6 @@
 /*data type are divided into two parts on the basis of they are kept and accessed from the memory*/
 
-/* Primitive:  call by value
+/* Primitive:  call by value uses:stack type of memory
         #1 String
         #2 Number
         #3 Boolean
@@ -9,7 +9,7 @@
         #6 Symbol :used to provide uniqueness to the component
         #7 BigInt: 12345657891229n
         
-    Non-Primitive: by reference
+    Non-Primitive: by reference  uses:heap type of memory
         #1 Array
         #2 Objects typeof function
         #3 Functions
@@ -33,3 +33,21 @@ let myOBj={
 const myFunction= function(){
     console.log();
 }
+//stack allocation reference changed
+let myName="Amit"
+let anotherName=myName
+anotherName="King"
+
+console.log(myName)
+console.log(anotherName)
+
+//heap allocation reference changed
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo=userOne
+userTwo.email="amit@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
