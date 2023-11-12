ff446cdadcab04b9825f2e889d2e5fba99e33516
AmitUpadhyay2020 <149964489+AmitUpadhyay2020@users.noreply.github.com>
2023-11-12 18:18:25 +0000

    maths

---
+++ 01_basics/07_maths.js
@@ -0,0 +1,19 @@
+const score=100
+const balance= new Number(100)
+console.log(balance)
+console.log(score)
+console.log(balance.toFixed(2)) //2 places after the decimal
+
+
+//Maths
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))  //takes upper value
+console.log(Math.floor(4.2))  //takes lower value
+
+console.log(Math.floor(Math.random()*10) +1)
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
