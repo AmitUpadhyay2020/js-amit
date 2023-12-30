90443d93dc54e203d929525ec2d7cc1fb76d2905
AmitUpadhyay2020 <149964489+AmitUpadhyay2020@users.noreply.github.com>
2023-12-30 08:47:12 +0000

    new

---
+++ 05_loops/filtermap.js
@@ -0,0 +1,9 @@
+const Coding=["JS","RUBY","JAVA","PYTHON"]
+Coding.forEach((item)=>{
+   console.log(item)
+})
+// it does not return anything
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNums.filter((num)=>num>4)
+console.log(newNums)
